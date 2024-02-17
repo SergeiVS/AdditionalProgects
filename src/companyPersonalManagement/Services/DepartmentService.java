6baf23debd561085d5f8d48f97e921d2894ccc08
SergeiVS <fam.smirnoff@gmail.com>
2024-02-17 20:10:39 +0100

    commit

--- src/companyPersonalManagement/Services/DepartmentService.java
+++ src/companyPersonalManagement/Services/DepartmentService.java
@@ -16,46 +16,52 @@ public class DepartmentService {
     private final DepartmentRepositoryService service;
     private final DepartmentRepository repository;
 
-
-
-Validation newDepValidation = new Validation() {
-    @Override
-    public List<ErrorDto> validate(Object request) {
-        List<ErrorDto> error = new ArrayList<>();
-        DepartmentDto dto;
-        String name;
-
-        if(request == null) {
-            error.add(new ErrorDto(ErrorCodes.ER401, "Request should not be empty"));
-            return error;
-        } else {
-            try {
-                dto = (DepartmentDto) request;
-                name = dto.getDepartmentName();
-            } catch (ClassCastException e) {
-                error.add(new ErrorDto(ErrorCodes.ER402, "Request in wrong format"));
-                return error;
-            }
-        }
-            if (repository.getDepartmentsRepository().containsKey(name)) {
-
-            }
-
-        if (name.length() < 3 || name.length() > 20){
-            error.add(new ErrorDto(ErrorCodes.ER405, "Name length must be between 3 and 20 characters"));
-            return error;
-        }
-        return error;
-    }
-};
-
-    public AddNewUnitDto addNewDepartment(NewDepartmentDto dto){
-
-        repository.addNewDepartment(dto);
-    }
-}
-
     public DepartmentService(DepartmentRepositoryService service, DepartmentRepository repository) {
         this.service = service;
         this.repository = repository;
     }
+
+    Validation newDepValidation = new Validation() {
+        @Override
+        public List<ErrorDto> validate(Object request) {
+            List<ErrorDto> error = new ArrayList<>();
+            DepartmentDto dto;
+            String name;
+
+            if (request == null) {
+                error.add(new ErrorDto(ErrorCodes.ER401, "Request should not be empty"));
+                return error;
+            } else {
+                try {
+                    dto = (DepartmentDto) request;
+                    name = dto.getDepartmentName();
+                } catch (Exception e) {
+                    error.add(new ErrorDto(ErrorCodes.ER402, "Request in wrong format"));
+                    return error;
+                }
+            }
+            if (repository.getDepartmentsRepository().containsKey(name)) {
+                error.add(new ErrorDto(ErrorCodes.ER403, "Name already exist"));
+                return error;
+            }
+
+            if (name.length() < 3 || name.length() > 20) {
+                error.add(new ErrorDto(ErrorCodes.ER405, "Name length must be between 3 and 20 characters"));
+                return error;
+            }
+            return error;
+        }
+    };
+
+    public AddNewUnitDto addNewDepartment(NewDepartmentDto dto) {
+        List<ErrorDto> errors = newDepValidation.validate(dto);
+        if (errors.isEmpty()) {
+            service.addNewDepartment(dto);
+            return new AddNewUnitDto("Department was added successful", errors);
+        } else {
+            return new AddNewUnitDto("Error, department did not added", errors);
+        }
+    }
+
+}
+
